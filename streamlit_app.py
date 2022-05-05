1624bcfc3cd356dbd4e862618eb385399d1cf70f
shaunaksn <88705974+shaunaksn@users.noreply.github.com>
2022-05-05 13:49:46 +0530

    Update streamlit_app.py
    Added items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
-import streamlit as st
+import streamlit
 
-st.title('My Parents new healthy diner')
+streamlit.title('My Parents new healthy diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
