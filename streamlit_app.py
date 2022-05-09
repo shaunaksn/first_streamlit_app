083503419b45a34be45bfa9fba7f6d4c29461aa0
shaunaksn <88705974+shaunaksn@users.noreply.github.com>
2022-05-09 21:32:17 +0530

    Update streamlit_app.py
    More refactoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    return fruityvice_normalized
 
 #New Section to show Fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
@@ -34,21 +38,27 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
-    fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(this_fruit_choice)
+    streamlit.dataframe(back_from_function)
+
 except URLError as e:
   streamlit.error()
+
 #Don't run anything past here
 streamlit.stop()
 
-#Connecting to Snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains")
-streamlit.dataframe(my_data_rows)
+#Snowflake Related Functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur
+    my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+    return my_data_rows = my_cur.fetchall()
+
+#Add button to snowflake
+if streamlit.button('Get Fruit Load List')
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 #New textbox
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
