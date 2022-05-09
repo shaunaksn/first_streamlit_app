510a3bee0ec43f6227edc21cb31f01a18848aad7
shaunaksn <88705974+shaunaksn@users.noreply.github.com>
2022-05-09 12:24:51 +0530

    Update streamlit_app.py
    Added Response section

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+#New Section to show Fruityvice response
+import requests
+fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
