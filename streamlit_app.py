f5f8ad71285b3f1a94a894e4d2156cef128a6c13
shaunaksn <88705974+shaunaksn@users.noreply.github.com>
2022-05-09 16:06:58 +0530

    Update streamlit_app.py
    Added second text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,6 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains")
 streamlit.dataframe(my_data_rows)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?' , 'Kiwi')
+streamlit.write('Thanks for adding', fruit_choice)
