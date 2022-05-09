456a86a78b127cbf83183871686f83f64149c7ee
shaunaksn <88705974+shaunaksn@users.noreply.github.com>
2022-05-09 16:24:06 +0530

    Update streamlit_app.py
    checking control flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,5 @@ streamlit.dataframe(my_data_rows)
 #New textbox
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit');")
